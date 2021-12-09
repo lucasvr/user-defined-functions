de7ce108f63fd91b717bb098ea9e06a74d855869
Lucas C. Villa Real <lucasvr@gmail.com>
2021-12-09 10:09:44 -0300

    Linux: improved console msg retrieval via QEMU

--- fun/linux_on_hdf5/linux.py
+++ fun/linux_on_hdf5/linux.py
@@ -4,19 +4,18 @@ def dynamic_dataset():
     import subprocess, os
     from telnetlib import Telnet
 
-    # Read the initramfs dataset
+    # Read the initramfs and kernel datasets
     initramfs = lib.getData("initramfs")
     initramfs_size = lib.getDims("initramfs")[0]
     with open(".initramfs", "wb") as f:
         f.write(bytes(initramfs[0:initramfs_size]))
 
-    # Read the kernel dataset
     kernel = lib.getData("kernel")
     kernel_size = lib.getDims("kernel")[0]
     with open(".kernel", "wb") as f:
         f.write(bytes(kernel[0:kernel_size]))
 
-    # Launch QEMU with a telnet-based console
+    # Launch the QEMU hypervisor with a telnet-based console
     cmd = [
         "qemu-system-x86_64",
         "-nographic",
@@ -25,10 +24,9 @@ def dynamic_dataset():
         "-initrd", ".initramfs",
         "-monitor", "/dev/null",
         "-serial", "telnet:localhost:4321,server,wait",
+        "-append", "notsc",
     ]
     p = subprocess.Popen(cmd, stderr=subprocess.PIPE)
-
-    # Wait until the telnet service is up
     while True:
         try:
             tn = Telnet("localhost", 4321)
@@ -37,10 +35,19 @@ def dynamic_dataset():
             pass
 
     # Write each line retrieved from telnet to the console dataset
-    line_nr = 0
     console = lib.getData("console")
     console_size = lib.getDims("console")[0]
 
+    lines = read_lines(0, console_size, tn)
+    for line_nr,line in enumerate(lines):
+        lib.setString(console[line_nr], line)
+
+    p.terminate()
+    os.unlink(".kernel")
+    os.unlink(".initramfs")
+
+def read_lines(line_nr, console_size, tn):
+    result = []
     while line_nr < console_size:
         lines = tn.read_until(b"\r\n", 0.5)
         if len(lines) == 0:
@@ -52,9 +59,6 @@ def dynamic_dataset():
             elif line.find(b"\033[2J") >= 0:
                 line = line.split(b"\033[2J")[1]
             if len(line):
-                lib.setString(console[line_nr], line)
+                result.append(line)
                 line_nr += 1
-
-    p.terminate()
-    os.unlink(".kernel")
-    os.unlink(".initramfs")
+    return result
